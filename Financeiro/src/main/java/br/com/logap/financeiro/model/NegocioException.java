2a0d1ad771c2c6ac4b42e94bd116278c34b3cdea
deangeli <deangeli@10.7.193.137>
2015-07-10 17:37:38 -0300

    update

--- Financeiro/src/main/java/br/com/logap/financeiro/model/NegocioException.java
+++
@@ -1,9 +0,0 @@
-package br.com.logap.financeiro.model;
-
-public class NegocioException extends Exception{
-	private static final long serialVersionUID = 1L;
-	
-	public NegocioException(String msg) {
-		super(msg);
-	}
-}
