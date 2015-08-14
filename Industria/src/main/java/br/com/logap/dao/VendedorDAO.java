2ce6a2d2bdd36ca8890f59aa86a63405672ae892
mypc <mypc@pc>
2015-08-14 10:51:36 -0300

    vendedotDAO update

--- Industria/src/main/java/br/com/logap/dao/VendedorDAO.java
+++ Industria/src/main/java/br/com/logap/dao/VendedorDAO.java
@@ -1,52 +1,69 @@
 package br.com.logap.dao;
 
+
+
+import java.util.List;
+
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
-
-import br.com.logap.modelo.Venda;
+import br.com.logap.modelo.Vendedor;
 
 public class VendedorDAO{
 	
 	
 	
 	
-	public void inserir(Venda venda, SessionManager sessionManager){
+	public void inserir(Vendedor vendedor, SessionManager sessionManager){
 		Session session = sessionManager.getSession();
 		Transaction transaction = session.beginTransaction();
 		
-		session.saveOrUpdate(venda);
+		session.saveOrUpdate(vendedor);
 		transaction.commit();
 		session.close();
 	} 
 	
 	
 	
-	public void deletar(Venda venda, SessionManager sessionManager){
+	public void deletar(Vendedor vendedor, SessionManager sessionManager){
 		Session session = sessionManager.getSession();
 		Transaction transaction = session.beginTransaction();
 		
-		session.delete(venda);
+		session.delete(vendedor);
 		transaction.commit();
 		session.close();
 	}
 	
-	public void Modificar(Venda venda, SessionManager sessionManager){
+	public void Modificar(Vendedor vendedor, SessionManager sessionManager){
 		Session session = sessionManager.getSession();
 		Transaction transaction = session.beginTransaction();
 		
-		session.saveOrUpdate(venda);
+		session.saveOrUpdate(vendedor);
 		transaction.commit();
 		session.close();
 	}
 	
 	
-	public void buscarTodos(SessionManager sessionManager){
+	public List<Vendedor> buscarTodos(SessionManager sessionManager){
+		
+		String HQL = "From Vendedor";
+		Session session = sessionManager.getSession();
+		Query query = (Query) session.createQuery(HQL);
+		List<Vendedor>vendedores = query.list();
+		return vendedores;
+		
+	}
+	
+	
+	public Vendedor buscarPorID(SessionManager sessionManager, long vendedorID){
+		
+		String HQL = "From Vendedor v where v.id = :vendedor_id";
 		
 		Session session = sessionManager.getSession();
-		Transaction transaction = session.beginTransaction();
+		Query query = (Query) session.createQuery(HQL);
+		query.setLong("vendedor_id", vendedorID);
 		
-		
-		
+		return (Vendedor) query.uniqueResult();		
 	}
 
 	
