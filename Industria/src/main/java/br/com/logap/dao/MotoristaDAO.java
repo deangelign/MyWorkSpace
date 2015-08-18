e9aec39766c0e293d039f310c6896b1096e13b21
mypc <mypc@192.168.1.136>
2015-08-18 17:07:32 -0300

    18/08/2015 deangeli

--- Industria/src/main/java/br/com/logap/dao/MotoristaDAO.java
+++ Industria/src/main/java/br/com/logap/dao/MotoristaDAO.java
@@ -1,65 +1,9 @@
 package br.com.logap.dao;
 
-import java.util.List;
-
-import org.hibernate.Query;
-import org.hibernate.Session;
-import org.hibernate.Transaction;
-
-import br.com.logap.modelo.Motorista;
 
 public class MotoristaDAO extends DAO{
 
 	public MotoristaDAO(){
 		super();		
-	}
-			
-	public void inserir(Motorista motorista){
-		Session session = SessionManager.getSession();
-		Transaction transaction = session.beginTransaction();
-		
-		session.saveOrUpdate(motorista);
-		transaction.commit();
-		session.close();
-	}
-		
-	public void deletar(Motorista motorista){
-		Session session = SessionManager.getSession();
-		Transaction transaction = session.beginTransaction();
-		
-		session.delete(motorista);
-		transaction.commit();
-		session.close();
-	}
-	
-	public void Modificar(Motorista motorista){
-		Session session = SessionManager.getSession();
-		Transaction transaction = session.beginTransaction();
-		
-		session.saveOrUpdate(motorista);
-		transaction.commit();
-		session.close();
-	}
-	
-	public List<Motorista> buscarTodos(){
-		
-		String HQL = "From motorista";
-		Session session = SessionManager.getSession();
-		Query query = (Query) session.createQuery(HQL);
-		List<Motorista>motoristaes = query.list();
-		return motoristaes;
-		
-	}
-	
-	public Motorista buscarPorID(long motoristaID){
-		String HQL = "From motorista v where v.id = :motorista_id";
-		
-		Session session = SessionManager.getSession();
-		Query query = (Query) session.createQuery(HQL);
-		query.setLong("motorista_id", motoristaID);
-		
-		return (Motorista) query.uniqueResult();		
-	}
-	
-	
+	}	
 }
--- Industria/src/main/java/br/com/logap/dao/ProdutoDAO.java
+++ Industria/src/main/java/br/com/logap/dao/ProdutoDAO.java
@@ -1,12 +1,5 @@
 package br.com.logap.dao;
 
-import java.util.List;
-
-import org.hibernate.Query;
-import org.hibernate.Session;
-import org.hibernate.Transaction;
-
-import br.com.logap.modelo.Produto;
 
 public class ProdutoDAO extends DAO{
 
@@ -14,48 +7,5 @@ public class ProdutoDAO extends DAO{
 		super();		
 	}
 
-	public void inserir(Produto produto){
-		Session session = SessionManager.getSession();
-		Transaction transaction = session.beginTransaction();
 
-		session.saveOrUpdate(produto);
-		transaction.commit();
-		session.close();
-	}
-
-	public void deletar(Produto produto){
-		Session session = SessionManager.getSession();
-		Transaction transaction = session.beginTransaction();
-
-		session.delete(produto);
-		transaction.commit();
-		session.close();
-	}
-
-	public void Modificar(Produto produto){
-		Session session = SessionManager.getSession();
-		Transaction transaction = session.beginTransaction();
-
-		session.saveOrUpdate(produto);
-		transaction.commit();
-		session.close();
-	}
-
-	public List<Produto> buscarTodos(){
-		String HQL = "From Produto";
-		Session session = SessionManager.getSession();
-		Query query = (Query) session.createQuery(HQL);
-		List<Produto>produtos = query.list();
-		return produtos;
-	}
-
-	public Produto buscarPorID(long produtoID){
-		String HQL = "From Produto v where v.id = :produto_id";
-
-		Session session = SessionManager.getSession();
-		Query query = (Query) session.createQuery(HQL);
-		query.setLong("produto_id", produtoID);
-
-		return (Produto) query.uniqueResult();		
-	}
 }
