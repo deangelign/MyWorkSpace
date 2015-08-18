24ba5562291d21e659d4531c19c815e19c41fe15
DELL <DELL@10.5.20.255>
2015-08-18 15:54:03 -0300

    Pessoa

---
+++ Industria/src/main/java/br/com/logap/modelo/Pessoa.java
@@ -0,0 +1,39 @@
+package br.com.logap.modelo;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public class Pessoa {
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	@Column(name="id_pessoa")
+	private long id;
+	
+	@Column(name="nome_pessoa", nullable=false)
+	private String nome;
+	
+	@Column(name="tipo_pessoa",nullable=false)
+	private String tipoPessoa;
+	
+	public Pessoa(){}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
