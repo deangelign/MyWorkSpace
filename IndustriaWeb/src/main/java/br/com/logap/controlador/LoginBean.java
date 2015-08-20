ce20d6a35f81278675a317d968a3de82455abd23
DELL <DELL@192.168.1.111>
2015-08-20 15:05:14 -0300

    20/08/2015 - inicio do web

---
+++ IndustriaWeb/src/main/java/br/com/logap/controlador/LoginBean.java
@@ -0,0 +1,63 @@
+package br.com.logap.controlador;
+
+import br.com.logap.dao.HibernateDAO;
+import br.com.logap.modelo.Usuario;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean(name = "loginBean")
+@SessionScoped
+public class LoginBean {
+
+	private Usuario usuario;
+
+	private String login;
+	private String senha;
+
+	public LoginBean() {
+		
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	public void Logar() {
+		login = usuario.getLogin();
+		senha = usuario.getSenha();
+		
+		validarUsuario(login, senha);
+	}
+
+	public String validarUsuario(String login, String senha) {
+		String usuarioTeste = "teste";
+		String senhaTeste = "1234";
+
+		if ((usuario.equals(usuarioTeste)) && (senha.equals(senhaTeste))){
+			return "inicio.xhtml";
+		}else{
+			return "login.xhtml";
+		}
+	}
+}
