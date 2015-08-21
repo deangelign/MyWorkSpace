bf628021f1f755cafcd208f1bc01b68b31d35fac
Desktop <Desktop@192.168.1.113>
2015-08-20 21:54:52 -0300

    20/08/2015 - xhtml - julio

---
+++ IndustriaWeb/src/main/java/br/com/logap/controlador/CadastrarProdutoBean.java
@@ -0,0 +1,10 @@
+package br.com.logap.controlador;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean(name = "cadastrarProdutoBean")
+@SessionScoped
+public class CadastrarProdutoBean {
+
+}
---
+++ IndustriaWeb/src/main/java/br/com/logap/controlador/CadastrarVendaBean.java
@@ -0,0 +1,12 @@
+package br.com.logap.controlador;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean(name = "cadastrarVendaBean")
+@SessionScoped
+public class CadastrarVendaBean {
+	
+	
+
+}
