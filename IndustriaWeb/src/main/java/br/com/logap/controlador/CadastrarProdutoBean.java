dba3955c40c556b6b2ea306f0f6113a8cdbdadbc
DELL <DELL@192.168.1.111>
2015-08-22 17:11:11 -0300

    Modificações

--- IndustriaWeb/src/main/java/br/com/logap/controlador/CadastrarProdutoBean.java
+++ IndustriaWeb/src/main/java/br/com/logap/controlador/CadastrarProdutoBean.java
@@ -3,8 +3,56 @@ package br.com.logap.controlador;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
+import br.com.logap.dao.AlmoxarifadoDAO;
+import br.com.logap.dao.ProdutoDAO;
+import br.com.logap.modelo.Almoxarifado;
+import br.com.logap.modelo.Produto;
+
 @ManagedBean(name = "cadastrarProdutoBean")
 @SessionScoped
 public class CadastrarProdutoBean {
+	private Produto produto;
+	private ProdutoDAO daoP;
+	
+	private Almoxarifado almoxarifado;
+	private AlmoxarifadoDAO daoA;
+	
+	public CadastrarProdutoBean(){
+		produto = new Produto();
+		almoxarifado = new Almoxarifado();
+		daoP = new ProdutoDAO();
+		daoA = new AlmoxarifadoDAO();
+	}
 
+	public Produto getProduto() {
+		return produto;
+	}
+
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
+
+	public Almoxarifado getAlmoxarifado() {
+		return almoxarifado;
+	}
+
+	public void setAlmoxarifado(Almoxarifado almoxarifado) {
+		this.almoxarifado = almoxarifado;
+	}
+	
+	public void cadastraProduto(){
+		daoP.inserir(produto);
+	}
+	
+	public void removerProduto(){
+		daoP.remover(produto);
+	}
+	
+	public void atualizarProduto(){
+		daoP.atualizar(produto);
+	}
+	
+	public void listarProdutos(){
+		daoP.buscarTodos();		
+	}
 }
--- IndustriaWeb/src/main/java/br/com/logap/controlador/CadastrarVendaBean.java
+++ IndustriaWeb/src/main/java/br/com/logap/controlador/CadastrarVendaBean.java
@@ -3,10 +3,56 @@ package br.com.logap.controlador;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
+import br.com.logap.dao.AlmoxarifadoDAO;
+import br.com.logap.dao.ClienteDAO;
+import br.com.logap.dao.ProdutoDAO;
+import br.com.logap.dao.VendedorDAO;
+import br.com.logap.modelo.Almoxarifado;
+import br.com.logap.modelo.Cliente;
+import br.com.logap.modelo.Produto;
+import br.com.logap.modelo.Venda;
+import br.com.logap.modelo.Vendedor;
+
 @ManagedBean(name = "cadastrarVendaBean")
 @SessionScoped
 public class CadastrarVendaBean {
+	private Venda venda;
 	
+	private Vendedor vendedor;
+	private VendedorDAO daoV;
 	
+	private Cliente almoxarifado;
+	private ClienteDAO daoC;
+	
+	public CadastrarVendaBean(){
+		venda = new Venda();
+		vendedor = new Vendedor();
+		daoV = new VendedorDAO();
+		daoC = new ClienteDAO();
+	}
+
+	public Venda getVenda() {
+		return venda;
+	}
+
+	public void setVenda(Venda venda) {
+		this.venda = venda;
+	}
+
+	public Vendedor getVendedor() {
+		return vendedor;
+	}
+
+	public void setVendedor(Vendedor vendedor) {
+		this.vendedor = vendedor;
+	}
+
+	public Cliente getAlmoxarifado() {
+		return almoxarifado;
+	}
+
+	public void setAlmoxarifado(Cliente almoxarifado) {
+		this.almoxarifado = almoxarifado;
+	}
 
 }
